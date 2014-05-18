c632de62b1dbaaae68fefff7df75a128257457bf
George Czabania <george@czabania.com>
2014-05-18 19:53:35 +1200

    Ditch browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
-var _ = require('lodash');
 var gulp = require('gulp');
-var brfs = require('brfs');
-var browserify = require('browserify');
+var brfs = require('gulp-brfs');
 var source = require('vinyl-source-stream');
-var uglify = require('gulp-uglify');
-var streamify = require('gulp-streamify');
 
 gulp.task('default', ['bundle']); 
 
@@ -14,15 +10,9 @@ gulp.task('default', ['bundle']);
  */
 
 gulp.task('bundle', function () {
-  return browserify()
-  .add('./index.js')
-  .exclude('lodash')
-  .transform(brfs)
-  .bundle({ standalone: 'termColors' })
-  .on('error', console.log.bind(console))
-  .pipe(source('bundle.min.js'))
-  .pipe(streamify(uglify()))
-  .pipe(gulp.dest('./'));
+  return gulp.src('lib/**/*.js', {buffer: false})
+  .pipe(brfs())
+  .pipe(gulp.dest('pkg'));
 });
 
 
